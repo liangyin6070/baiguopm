17a0d0a5a19e7dd9f526ff5d105459437bcddd8d
liudewei <liudw2@163.com>
2017-03-22 16:17:14 +0800

    xiu

--- src/main/java/com/baiguo/framework/wechat/unstoppedable/common/Configure.java
+++ src/main/java/com/baiguo/framework/wechat/unstoppedable/common/Configure.java
@@ -11,23 +11,23 @@ import java.util.Properties;
  */
 public class Configure {
 	//微信支付回调url
-	private static String notifyUrl = "http://testwx.run2smart.cn/wechats/wxPay/success.htm";
+	private static String notifyUrl = "";
 	//这个就是自己要保管好的私有Key了（切记只能放在自己的后台代码里，不能放在任何可能被看到源代码的客户端程序中）
 	// 每次自己Post数据给API的时候都要用这个key来对所有字段进行签名，生成的签名会放在Sign这个字段，API收到Post数据的时候也会用同样的签名算法对Post过来的数据进行签名和验证
 	// 收到API的返回的时候也要用这个key来对返回的数据算下签名，跟API的Sign数据进行比较，如果值不一致，有可能数据被第三方给篡改
-	private static String key = "c65d60e715f1491ab43ffde6c51deb5a";
+	private static String key = "";
 	//微信分配的公众号ID（开通公众号之后可以获取到）
-	private static String appID = "wx361aa2ad90309c10";
+	private static String appID = "";
 	//微信公众号密钥
-	private static String appSecret = "ed1079249d5ae5196048323cb1ce65b1";
+	private static String appSecret = "";
 	//微信支付分配的商户号ID（开通公众号的微信支付功能之后可以获取到）
-	private static String mchID = "1369741402";
+	private static String mchID = "";
 	//受理模式下给子商户分配的子商户号
 	private static String subMchID = "";
 	//HTTPS证书的本地路径
-	private static String certLocalPath = "apiclient_cert.p12";
+	private static String certLocalPath = "";
 	//HTTPS证书密码，默认密码等于商户号MCHID
-	private static String certPassword = "1369741402";
+	private static String certPassword = "";
 	//是否使用异步线程的方式来上报API测速，默认为异步模式
 	private static boolean useThreadToDoReport = true;
 	//机器IP
